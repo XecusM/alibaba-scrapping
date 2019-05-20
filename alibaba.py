160c74f879f4784bdfb5cd08e5aba0e795cad17c
Xecus <abo.elfotouh@live.com>
2019-05-20 14:00:55 +0200

    finished scrapping script and modify alibaba script

--- alibaba.py
+++ alibaba.py
@@ -1,2 +1,30 @@
 import time
 import pandas as pd
+import scrapping
+
+def Items(items):
+    # intiate items dataframe
+    Results = list()
+    for item in items:
+        print(item)
+        GetItem = {'item':item['Items'],
+                'link':'https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText={0}'.format(item['Items'].replace(' ','+')),
+                'number':item['Search']}
+        # get all search results for the given item
+        Results.append(scrapping.ItemResults(GetItem))
+
+def main():
+    check = True
+    while check:
+        file = input('Please, enter file name .csv to scrap its list: ')
+        try:
+            items = pd.read_csv(file)
+            check = False
+        except:
+            print("File not FOUND!\nTry AGAIN!\n'")
+    results = Items(items)
+    print(results)
+
+
+if __name__ == '__main__':
+    main()
--- scrapping.py
+++ scrapping.py
@@ -1,14 +1,17 @@
 import sys
+import re
 import requests
 from bs4 import BeautifulSoup as soup
-import time
 import pandas as pd
-
-# https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText=Huawei+P30+Lite
 
 def ItemResults(item):
     '''
     Function for scrapping list of items available for the desired one.
+    The function scrapping:
+        - Item name
+        - Item link
+        - Item price per piece
+        - Minimum order quantity
     '''
     #Connect to the web-site and scrapping top items details
     try :
@@ -16,12 +19,44 @@ def ItemResults(item):
     except:
         print('Error in reading page!')
         quit()
+    #get all contents
+    page = soup(url.content, 'html.parser')
+    # Find result items div
+    containers = page.findAll('div',{'class':'item-info'})
+    # intiate items dataframe
+    items = pd.DataFrame(columns=['item','link','price','min-order'])
+    # help counter for getting links
+    counter = 0
+    # get all details for every item result
+    for con in containers:
+        link = con.find('a')
+        price = page.find('div',{'class':'price'})
+        min_order = page.find('div',{'class':'min-order'})
+        # Add item details to dataframe
+        items.loc[counter] = [re.sub('\s+',' ',link.text.strip()),
+                            'https:{0}'.format(link['href']),
+                            re.sub('\s+',' ',price.text.strip()),
+                            re.sub('\s+',' ',min_order.text.strip())]
+        # counter for the next link
+        counter += 1
+        if counter >= item['number']:
+            break
+    return items
 
 def main(ItemName):
-    item = {'item':ItemName,'link':'https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText={0}'.format(ItemName.replace(' ','+'))}
+    '''
+    This main function to test the script alone
+    '''
+    # create link for the given item name
+    item = {'item':ItemName,
+            'link':'https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText={0}'.format(ItemName.replace(' ','+')),
+            'number':10}
     print(item)
-    # Results = ItemResults(item)
-    # print(Results)
+    # get all search results for the given item
+    Results = ItemResults(item)
+    print(Results)
 
 if __name__ == '__main__':
+    # get the item name as string in the command line
+    # after calling the script
     main(sys.argv[1])
